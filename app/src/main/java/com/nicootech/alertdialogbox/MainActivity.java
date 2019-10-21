ad686587155ee3d11078185ae39b5199f2d0e658
Ehsan <ehrath1@gmail.com>
2019-10-21 15:15:13 -0700

    first commit

---
+++ app/src/main/java/com/nicootech/alertdialogbox/MainActivity.java
@@ -0,0 +1,40 @@
+package com.nicootech.alertdialogbox;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+        adb.setMessage("Do You Really Want to Exit?")
+                .setCancelable(true)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                })
+
+                .setTitle("WAIT");
+        adb.show();
+
+
+    }
+}
